1e0828c72419990ecda6f8807445a343d2e459b4
Naddiya Sheraz <naddiyasheraz18@gmail.com>
2025-01-17 15:06:54 +0500

    Updated App.css and App.jsx with latest changes

--- Input-mirror/src/App.jsx
+++ Input-mirror/src/App.jsx
@@ -9,7 +9,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [InputValue, setInputValue] = useState("nil")
+  const [InputValue, setInputValue] = useState("")
 
   let GetFirstName = (event) =>{
     setInputValue (event.target.value);
@@ -19,9 +19,9 @@ function App() {
 
   return (
     <>
-     <input type="text" onChange={GetFirstName} value={InputValue} placeholder='Enter your FirtName' /> <br />
+     <input style={{color:"#853444", width: "300px", padding: "10px", marginBottom: "10px", borderRadius: "5px", border: "1px solid #ccc", fontSize: "16px", outline: "none", boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)", transition: "all 0.3s ease",}} type="text" onChange={GetFirstName} value={InputValue} placeholder='Write' /> <br />
      
-    <p>you typed :{InputValue}</p>
+    <p style={{color: "#CF3455", fontWeight: "bold", fontSize: "20px", marginTop: "10px",}}>you typed :{InputValue}</p>
     </>
   )
 }
